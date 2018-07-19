9a8f2e1f86780beb5e6908090331105b32ef6094
mholliday6611 <mholliday6611@gmail.com>
2018-07-18 22:26:44 -0400

    procfile setup

--- crazy8sclient/src/index.js
+++ crazy8sclient/src/index.js
@@ -7,9 +7,11 @@ import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom"
 
 ReactDOM.render(
+	<div>
 	<Router>
 		<Route path="/" component={Home} />
 		<Route exact path="/:room" component={App} />
-	</Router>, 
+	</Router>
+	</div>, 
 	document.getElementById('root'));
 registerServiceWorker();
